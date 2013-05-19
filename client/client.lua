9a57c43d233b2a6419e105754a203f092b532334
itzmjauz <itzmjauz@gmail.com>
2013-05-19 23:17:00 +0200

    fix

--- client/client.lua
+++ client/client.lua
@@ -1,8 +1,6 @@
 rednet.open('top')
 
 while true do
-  event, scancode = os.pullEvent("key")
-  if tostring(scancode)=="a" then
-    rednet.broadcast("that's a penis!")    
-  end
+  rednet.broadcast('thats a penis')
+  sleep(3)
 end
--- server/server.lua
+++ server/server.lua
@@ -1,8 +1,6 @@
 -- attach to upper screen and reboot it ' left '
-screen = peripheral.wrap("left")
-screen.reboot()
-sid = screen.getID()
-print('reboot of: '+sid' done')
+term = peripheral.wrap('left')
+term.clear()
 -- open rednet ( on ' right' )
 rednet.open('right')
 rednet.broadcast('server on')
@@ -12,9 +10,13 @@ if fs.exists('data')==false then
 end
 -- init done, main loop:
 while true do
-  local event,p1,p2 = OS.pullEvent("rednet_receive")
-  term.clear()
-  term.write(p1 +': '+p2)
+  local event,p1,p2 = os.pullEvent("rednet_message")
+  term.write(tostring(p1))
+  term.write(' sends: ')
+  term.write(tostring(p2))
+  local cX,cY = term.getCursorPos()
+  term.setCursorPos(1,cY+1)
+
 end
 
 
